a04ba1580fd158d11d79db8463fdcd1264b2f958
CoMatu <30466272+CoMatu@users.noreply.github.com>
2019-04-04 22:34:03 +0500

    надо добавить JWT

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ const insertUser = async(ctx, next) => {
 
   const getUser = async(ctx, next) => {
     await next()
-    let name = ctx.params.name
+    let name = ctx.params.name // возможно будет ошибка, пока не проверял
 
     // Throw the error if no name.
     if (name === undefined) {
@@ -142,8 +142,50 @@ const insertUser = async(ctx, next) => {
 
   const login = async(ctx, next) => {
     await next()
+
+    let body = ctx.request.body || {}
+
+    let email = body.email
+    let password = body.password
+
+    // Throw the error if no name.
+    if (email === undefined) {
+      ctx.throw(400, 'name is required')
+    }
+  
+    // Get the db connection.
+    const connection = await db()
+  
+    // Throw the error if the table does not exist.
+    var exists = await r.tableList().contains('users').run(connection)
+    if (exists === false) {
+      ctx.throw(500, 'users table does not exist')
+    }
+  
+    let searchQuery = {
+      email: email
+    }
+  
+    // Retrieve documents by filter.
+    var user = await r.table('users')
+      .filter(searchQuery)
+      .nth(0) // query for a stream/array element by its position
+      .default(null) // will return null if no user found.
+      .run(connection)
+  
+    // Throw the error if no user found.
+    if (user === null) {
+      ctx.throw(400, 'такого пользователя не существует')
+    }
+
+    if (user.password === password) {
+
+      ctx.body = true
+
+    } else {
+      ctx.throw(400, 'пароль не верный')
+    }
 //    console.log(ctx.request.query)
-    ctx.body = 'получен запрос на авторизацию'
  }
 
 router
